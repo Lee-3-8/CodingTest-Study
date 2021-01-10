ff587632fc6787bbf60aa97d38baaac0410ba302
방규빈 <rbqls1057@naver.com>
2021-01-10 18:16:27 +0900

    Create 규빈.py

---
+++ level2/문자열_압축/규빈.py
@@ -0,0 +1,26 @@
+def solution(s):
+    answer = 0
+    change = 0
+
+    for i in range(1, (len(s)//2 + 2)):
+        change = stringSlice(s,i)
+        if i == 1 or answer > change:
+            answer = change
+    return answer
+
+def stringSlice(string, n) :
+    index = 0
+    result = ''
+    while(index < len(string)):
+        compare = string[index:index+n]
+        count = 0
+        while(compare == string[index:index+n]):
+            count += 1
+            index += n
+        if count > 1 :
+            result = result + str(count) + compare
+        else :
+            result = result + compare
+    return len(result)
+
+print(solution(""))

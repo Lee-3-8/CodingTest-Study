6f81ed627549a10254c8314b47eb6a8896454db3
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-01-18 02:55:44 +0900

    [level2] 소수만들기 수정

--- level2/소수_만들기/신필.py
+++ level2/소수_만들기/신필.py
@@ -1,6 +1,6 @@
 from itertools import combinations
 
-def isPrime(n):
+def isPrime1(n): # 무식 소수 
 	if(n<2):
 		return 0
 	for i in range(2,n):
@@ -8,27 +8,50 @@ def isPrime(n):
 			return 0
 	return 1
 
-# def solution(nums):
-#     n = len(nums)
-#     result = 0
-#     temp = [0,0,0]
-#     for i in range(n-2):
-#         temp[0] = nums[i]
-#         for j in range(i+1,n-1):
-#             temp[1] = nums[j]
-#             for k in range(j+1,n):
-#                 temp[2] = nums[k]
-#                 print(sum(temp))
-#                 result += isPrime(sum(temp))
-#     return result
+def isPrime2(n): # 에라토스테네스의 체 
+    a = [False,False] + [True]*(n-1)
+    primes=[]
+    for i in range(2,n+1):
+        if a[i]:
+            primes.append(i)
+        for j in range(2*i, n+1, i):
+            a[j] = False
+    return(primes)
 
 
-def solution(nums):
+def solution1(nums): # 처음푼거 
+    n = len(nums)
+    result = 0
+    temp = [0,0,0]
+    for i in range(n-2):
+        temp[0] = nums[i]
+        for j in range(i+1,n-1):
+            temp[1] = nums[j]
+            for k in range(j+1,n):
+                temp[2] = nums[k]
+                print(sum(temp))
+                result += isPrime1(sum(temp))
+    return result
+
+
+def solution2(nums): # 두번째
     re_arr = list(combinations(nums,3))
     re_arr = list(map(lambda x: sum(x) , re_arr))
     result = 0
     for i in re_arr:
-        result += isPrime(i)
+        result += isPrime1(i)
     return result
-print(solution([1,2,3,4]))
+
+def solution3(nums): # 에라토스테네스의 체 사용
+    re_arr = list(combinations(nums,3))
+    re_arr = list(map(lambda x: sum(x) , re_arr))
+    result = isPrime2(max(re_arr))
+    cnt = 0
+    for i in re_arr:
+        if i in result:
+            cnt += 1
+
+    return cnt
+
+print(solution3([1,2,3,4]))
 # print(solution([1,2,7,6,4]))

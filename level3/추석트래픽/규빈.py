f742643997a8bb7edb3a8d26b8f54c93b87b7c35
방규빈 <rbqls1057@naver.com>
2021-02-05 16:37:39 +0900

    Update 규빈.py

--- level3/추석트래픽/규빈.py
+++ level3/추석트래픽/규빈.py
@@ -1,7 +1,7 @@
 import re
 from datetime import datetime, timedelta
 def solution(lines):
-    answer = 1
+    answer = 0
     times = []
     for i in range(len(lines)):
         cTime, pTime = re.match(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} )(\d\.?\d*)', lines[i]).groups()
@@ -10,18 +10,18 @@ def solution(lines):
         processTime = datetime.strptime(pTime, '%Y-%m-%d %H:%M:%S.%f')
         startTime = endTime - processTime + timedelta(milliseconds=1)
         startTime = datetime(2016,9,15,0,0,0) + startTime
-        times.append([startTime, endTime])
+        times.append([startTime, i])
+        times.append([endTime, i])
     times = sorted(times, key= lambda x : (x[1],x[0]))
 
     for i in range(len(times) - 1):
-        for j in times[i]:
-            later = j + timedelta(seconds=1) - timedelta(milliseconds=1)
-            curNum = 1
-            for k in range(i+1, len(times)):
-                if later < times[k][0]:
-                    break
-                if (j <= times[k][0] and times[k][0] <= later) or (j <= times[k][1] and times[k][1] <= later) or (times[k][0] <= j and j <= times[k][1]):
-                    curNum += 1
-            answer = max(answer, curNum)
+        secLater = times[i][0] + timedelta(seconds=1) - timedelta(milliseconds=1)
+        curNum = 1
+        a = {times[i][1]}
+        for j in range(i+1, len(times)):
+            if times[j][1] not in a and secLater >= times[j][0]:
+                curNum += 1
+                a.add(times[j][1])
+        answer = max(answer, curNum)
 
     return answer

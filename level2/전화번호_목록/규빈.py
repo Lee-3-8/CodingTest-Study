19497212eec6b066f7582ba2f5ba32782a7a9a39
방규빈 <rbqls1057@naver.com>
2021-02-24 22:47:20 +0900

    Update 규빈.py

--- level2/전화번호_목록/규빈.py
+++ level2/전화번호_목록/규빈.py
@@ -2,6 +2,6 @@ def solution(phone_book):
     phone_book.sort(key=len)
     for i in range(len(phone_book)):
         for j in range(i+1,len(phone_book)):
-            if phone_book[i] == phone_book[j][:len(phone_book[i])]: # startwith
+            if phone_book[i] == phone_book[j][:len(phone_book[i])]: # starstwith
                 return False
     return True

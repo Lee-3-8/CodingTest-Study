486118c61e812df2ddd49804574d4d860574f7e2
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-03-10 12:13:51 +0900

    Revert "Revert "[스텍,큐 ] 다리를 지나는 트럭""
    This reverts commit 02a36bd4c301448d91d7e4ee3e133d2be76f2ac1.

--- level2/전화번호_목록/신필.py
+++ level2/전화번호_목록/신필.py
@@ -1,11 +1,10 @@
 def solution(phone_book):
-    arr_len =len(phone_book)
+    arr_len = len(phone_book)
     phone_book.sort(key = lambda x: len(x))
     for i in range(arr_len):
         for j in range(i+1,arr_len):
             if phone_book[i] == phone_book[j][0:len(phone_book[i])]:
                 return False
     return True
-
 
 print(solution(["97674223","119","1195524421"]))

f79ea5e28b2404277025ddf531a343b2e71a4ee8
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-03-10 12:13:25 +0900

    [스텍,큐 ] 다리를 지나는 트럭

--- level3/베스트앨범.py/신필.py
+++ level3/베스트앨범.py/신필.py
@@ -5,7 +5,7 @@ def solution(genres, plays):
     for i in range(len(genres)):
         music_dict[genres[i]] = music_dict.get(genres[i],[]) + [[i,plays[i]]]
         genre_sum[genres[i]] = genre_sum.get(genres[i],0) + plays[i]
-    
+
     for i in sorted(genre_sum.items(), key = lambda x:x[1], reverse=True):
         for index,value in enumerate(sorted(music_dict[i[0]], key= lambda x: x[1], reverse=True)):
             if index >= 2:

48dfbf4d5701a61762603deebb828b8460b17cae
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-04-04 01:49:19 +0900

    Update 신필.py

--- level3/단속카메라/신필.py
+++ level3/단속카메라/신필.py
@@ -1,15 +1,14 @@
 
 def solution(routes):
-    # min_r = min(map(lambda x: x[0],routes))
-    routes.sort(key = lambda x: x[0])
-    point = routes[0][0]
+    routes.sort(key = lambda x: x[1])
+    point = float('-inf')
     result = 0
     for start,end in routes:
+        print(start,end,point)
         if point < start:
             point = end
             result += 1
 
-
     return result
 
-print(solution([[-20,15], [-14,-5], [-18,-13], [-5,-3]]))
+print(solution(	[[-20, 15], [-14, -5], [-18, -13], [-5, -3]]))

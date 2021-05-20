d77a957b3da2c5516ea34df5680879694efb97a0
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-05-20 13:35:12 +0900

    Create 정한.py

---
+++ level3/입국심사/정한.py
@@ -0,0 +1,22 @@
+# 3레벨     코딩테스트 고득점 Kit     입국심사
+def solution(n, times):
+    l = 1
+    r = max(times) * n
+    while l <= r:
+        m = (l + r) // 2
+        num = 0
+        for time in times:
+            num += m // time
+            if num >= n:
+                break
+
+        if num < n:
+            l = m + 1
+        else:
+            r = m - 1
+
+    return l
+
+
+if __name__ == "__main__":
+    print(solution(6, [7, 10]))

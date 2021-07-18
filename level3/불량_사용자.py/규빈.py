c20fd52188267d5fbb5417c864d02dca93f5f8e8
방규빈 <rbqls1057@gmail.com>
2021-07-18 15:11:59 +0900

    Create 규빈.py

---
+++ level3/불량_사용자.py/규빈.py
@@ -0,0 +1,20 @@
+import re
+from itertools import product
+def solution(user_id, banned_id):
+    answer = []
+
+    arr = [[] for _ in range(len(banned_id))]
+
+    for i in range(len(banned_id)):
+        for name in user_id:
+            newID = banned_id[i].replace('*', '.')
+            result = re.match(newID, name)
+            if result and len(result.group()) == len(name):
+                arr[i].append(name)
+
+    for users in product(*arr):
+        s_user = set(users)
+        if len(s_user) == len(users):
+            answer.append(tuple(sorted(list(users))))
+
+    return len(set(answer))

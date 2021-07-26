2f3d3834f37924e305a7a6197294129dcc883a0d
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-07-26 22:48:05 +0900

    Update 신필.py

--- level2/게임_맵_최단거리/신필.py
+++ level2/게임_맵_최단거리/신필.py
@@ -1,22 +1,21 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(10000000)
+
 def solution(maps):
     n, m = len(maps[0]),len(maps)
-    for y in range(m):
-        for x in range(n):
-            if maps[y][x] == 1: maps[y][x] = float('inf')
-
+    move = [(-1,0),(0,-1),(1,0),(0,1)]
     def dfs(a,b):
-        if maps[-1][-1] != 1 and maps[b][a] >= maps[-1][-1] : return
-        move = [(-1,0),(0,-1),(1,0),(0,1)]
         for i, j in move:
-            x, y = a + i, j + b
+            x, y = a + i, b + j
             if 0 <= x < n and 0 <= y < m:
-                if maps[y][x] != 0:
-                    if maps[b][a] + 1 < maps[y][x]:
+                if maps[y][x] == 1 or maps[b][a] + 1 < maps[y][x]:
                         maps[y][x] = maps[b][a] + 1
                         dfs(x,y)
-    maps[0][0] = 1
+
     dfs(0,0)
-    return -1 if maps[-1][-1] == float('inf') else maps[-1][-1]
+
+    return -1 if maps[-1][-1] == 1 else maps[-1][-1]
 
 if __name__ == '__main__':
     print(solution(	[[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
